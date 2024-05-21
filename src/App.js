910464fe687ca11be52779ab83288e43f5da5292
Pcontreras98 <martin_contrerasn@hotmail.com>
2024-05-20 21:23:10 -0500

    añadir componentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import pcLogo from './imagenes/pabloContrerasLogo.png';
+import ListaDeTareas from './componentes/ListaDeTareas';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="aplicacion-tareas">
+      <div className='pc-logo-contenedor'>
+          <img
+            src={pcLogo}
+            className='pc-logo' />
+      </div>
+
+      <div className='tareas-lista-principal'>
+        <h1>Mis tareas</h1>
+        <ListaDeTareas/>
+      </div>
     </div>
   );
 }
